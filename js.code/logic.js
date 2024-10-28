8f6ce9dd01d8bf12e5a9761ed87556e7661c057c
Ilyas Karim <ilyas.datoo@gmail.com>
2024-10-28 20:48:54 +0500

    update the new code

---
+++ js.code/logic.js
@@ -0,0 +1,131 @@
+// console.log('hello world');
+
+// key words
+// reserved words
+
+// case sensitivity
+
+// variables
+// var variableName = anyValue;
+
+//  var fname="ahmed";
+//  console.log(fname);
+
+// declare of define a variable
+// var age;
+
+// assign a value to a variable
+// age=30;
+// console.log(age);
+
+// update value
+//  age=34;
+// console.log(age);
+
+// var city='gilgit';
+// console.log(city);
+
+// city='karachi';
+// console.log(city);
+
+// datatypes
+// primitive datatypes
+// non-primitive datatypes/reference datatypes
+
+// primitive datatypes
+// single value store
+// var city='karachi';
+// var age=30;
+
+// string
+// single character or group of characters
+// e.g 'a', 'abc', '123', 'true', 'false'
+// console.log('hello world');
+
+
+// numbers
+// 90-90, 0.5, 0, -0.5, -90
+// number will be used without strings 
+// console.log(90);
+
+// boolean
+// true, false
+// var is18=true;
+// console.log(is18);
+// var is19=false;
+// console.log(is19);
+
+// `undefined`
+// var city;
+// console.log(city);
+
+// null
+// var city=null;
+// console.log(city);
+
+
+// symbols
+// var a='one';
+// var b='one';
+
+// var uuid=Symbol('123');
+// var uuid2=Symbol('123');
+// console.log(uuid);
+
+// bigint
+// var num=9007199254740991n;
+// console.log(num);
+
+// var ab ="a"
+// console.log(ab);
+// ab="pak"
+// console.log(ab);
+
+// var is18=false;
+// console.log(is18);
+
+// camelCase
+// gilgitCityPakistan
+// gilgit_city_pakistan // snake_case
+
+// const
+// const city="karachi";
+// console.log(city);
+
+
+// var age = + prompt("Enter your age ") 
+
+// if(age >0 && age <= 10){ 
+    //  console.log("child")
+//  }
+//  else if(age > 10 && age <=18){
+    //  console.log("teenager")
+//  }
+//  else if(age > 18 && age <=30){
+    //  console.log("adult")
+//  }
+//  else if(age > 30 && age <=50){
+    //  console.log("middle age")
+//  }
+//  else if(age > 50 && age <=80){
+    //  console.log("old")
+//  }
+//  else if(age > 80 && age<=120){
+    //  console.log("very old")
+//  }
+//  else{console.log("invalid age...try again")}
+
+var table=5;
+
+for(var i=1; i<=10; i++){
+    console.log(table + " * " + i + " = " + table*i);
+}
+
+
+
+
+
+
+
+
+
