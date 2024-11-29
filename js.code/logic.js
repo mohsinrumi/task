0d8e6202a18447ef078e879d50db3485107665d2
mohsinrumi <mohsinrumi2@gmail.com>
2024-11-29 18:59:53 +0500

    Initial commit

--- js.code/logic.js
+++ js.code/logic.js
@@ -92,40 +92,59 @@
 // const city="karachi";
 // console.log(city);
 
+// var table=5;
+
+//
 
 // var age = + prompt("Enter your age ") 
 
 // if(age >0 && age <= 10){ 
-    //  console.log("child")
+//   console.log("child")
 //  }
-//  else if(age > 10 && age <=18){
-    //  console.log("teenager")
-//  }
-//  else if(age > 18 && age <=30){
-    //  console.log("adult")
+//   else if(age > 10 && age <=18){
+//   console.log("teenager")
+//   }
+//   else if(age > 18 && age <=30){
+//      console.log("adult")
 //  }
 //  else if(age > 30 && age <=50){
-    //  console.log("middle age")
+//      console.log("middle age")
 //  }
 //  else if(age > 50 && age <=80){
-    //  console.log("old")
+//      console.log("old")
 //  }
 //  else if(age > 80 && age<=120){
-    //  console.log("very old")
+//      console.log("very old")
 //  }
 //  else{console.log("invalid age...try again")}
 
-var table=5;
+// let fruits =["karachi", "gilgit" , "lahore" , "islamabad"];
 
-for(var i=1; i<=10; i++){
-    console.log(table + " * " + i + " = " + table*i);
-}
+// for(let i = 0  ;  i< fruits.length ; i++){
+//     console.log(i , fruits[i])
+// }
 
-
-
-
-
-
-
+// some
+// every
+// sort
+// reduce
 
 
+let heading= document.querySelector('.header')
+// heading.style.backgroundColor = 'orange';
+// heading.style.fontSize='50px';
+// heading.style.border='5px solid black';
+// heading.style.color='white';
+heading.innerText='hello, javascript learners';
+
+let paragraph= document.querySelector('.paragraph')
+paragraph.style.backgroundColor='Black';
+paragraph.style.fontSize='18px';
+// paragraph.style.border='10px solid white';
+paragraph.style.color='white';
+paragraph.innerText='java can change text and style easily';
+
+let footer=document.querySelector('.footer')
+footer.style.backgroundColor = 'grey';
+// footer.style.fontSize='30px'
+footer.innerText='thankyou for visiting'
